25101c86b9058ab51c58cd1f9531f16fad5f65bb
zachoryb98 <zbehrensmeyer@yahoo.com>
2023-09-07 20:34:22 -0500

    Player rotation updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,34 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5.0f;
 
     private Vector2 moveInput;
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = transform; // Cache the player's transform component.
+    }
 
     private void Update()
     {
         // Player movement using Input System
         Vector3 movement = new Vector3(moveInput.x, 0.0f, 0.0f) * moveSpeed * Time.deltaTime;
-        transform.Translate(movement);
+        playerTransform.Translate(movement);
+
+        // Rotate the player based on input
+        if (moveInput.x < 0)
+        {
+            // If moving left (A key pressed), rotate to face left
+            playerTransform.localScale = new Vector3(-1, 1, 1); // Flip the player horizontally
+        }
+        else if (moveInput.x > 0)
+        {
+            // If moving right (D key pressed), rotate to face right
+            playerTransform.localScale = new Vector3(1, 1, 1); // Reset the scale to face right
+        }
     }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        // Add debug log to check if this method is being called.
-        Debug.Log("Move input: " + moveInput);
     }
 }
